c758d0b2dd894b63bf0684aae0ffd310e6ded8eb
chodge <cliffe@resounding.ca>
2014-04-08 22:44:13 -0400

    tsc & mocha tests work with grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,29 @@
 ﻿module.exports = function (grunt) {
 
-    //grunt.initConfig({
-    //    pkg: grunt.file.readJSON('package.json')
-    //});
+    grunt.loadNpmTasks('grunt-mocha-test');
 
-    grunt.registerTask('default', []);
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        ts: {
+            module: {
+                cmd: 'node_modules\\.bin\\tsc.cmd',
+                args: ['--module', 'commonjs', 'src/calculator.ts']
+            }
+        },
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec'
+                },
+                src: ['test/*.js']
+            }
+        }
+    });
+
+    grunt.registerMultiTask('ts', function () {
+        var done = this.async();
+        grunt.util.spawn(this.data, done);
+    });
+    grunt.registerTask('all', ['ts:module', 'mochaTest:test']);
+    grunt.registerTask('default', ['all']);
 };
